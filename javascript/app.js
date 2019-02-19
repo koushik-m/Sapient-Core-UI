98bc283a6759a08d135880c0b553e11bc7479875
Koushik Mani <koushik-m@Koushiks-MacBook-Pro.local>
2019-02-19 12:30:24 +0530

    js basics

---
+++ javascript/app.js
@@ -0,0 +1,26 @@
+var x = 10;
+console.log(x);
+var y = 'hey';
+var z= ['john', 'doe'];
+// var fullname = function() {};
+// console.log(typeof(x));
+// console.log(typeof(y));
+// console.log(typeof z);
+// console.log(typeof fullname);
+// if(typeof(x) == 'string') {
+//     console.log(
+//         'yes it is a number'
+//     );
+// } else {
+//     console.log('no it is not')
+// }
+for(var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+console.log(i);
+
+// any variable is considered as window object
+function clickme() {
+    alert('hi');
+    test = 'tests';
+}
