3d756fed7c498715eba7892fe8c5fb4edd05ef2b
Koushik Mani <koushik-m@Koushiks-MacBook-Pro.local>
2019-02-19 14:10:16 +0530

    js basics

--- javascript/app2.js
+++ javascript/app2.js
@@ -1,2 +1,9 @@
 // var i = 10;
-console.log(test);
+// console.log(window);
+
+// document.getElementById() expects the id of the element
+// document.getElementsByClassName() expects the class name of the element
+// document.getElementsByTagName expects the tag name of the element
+
+var btn = document.getElementsByTagName('button');
+console.log(btn);
---
+++ javascript/register.js
@@ -0,0 +1,9 @@
+
+function test() {
+    alert(document.getElementById('name').value);
+    alert(document.getElementById('password').value);
+    alert(document.getElementById('country').value);
+    alert(document.getElementById('gender').value);
+
+    
+}
