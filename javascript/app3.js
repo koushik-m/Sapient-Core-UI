825875f174b3cdaf1364ea66a9029226ea29dda2
Koushik Mani <koushik-m@Koushiks-MacBook-Pro.local>
2019-02-20 09:33:17 +0530

    js basics

---
+++ javascript/app3.js
@@ -0,0 +1,14 @@
+"use strict";
+function test() {
+    return (document.getElementsByTagName('*'));
+}
+function clickButton() {
+    
+}
+test();
+
+
+// tab index
+// compilation phase
+// v8 engine
+// how js works
--- javascript/register.js
+++ javascript/register.js
@@ -3,7 +3,7 @@ function test() {
     alert(document.getElementById('name').value);
     alert(document.getElementById('password').value);
     alert(document.getElementById('country').value);
-    alert(document.getElementById('gender').value);
+    alert(document.getElementsByName('gender').value);
 
     
 }
