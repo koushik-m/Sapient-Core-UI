69f0a5e5039c0660a06dfa43110677baae5a3ed0
Koushik Mani <koushik-m@Koushiks-MacBook-Pro.local>
2019-02-22 12:58:19 +0530

    added code

---
+++ javascript/play.js
@@ -0,0 +1,38 @@
+// (function(){
+//     'use strict';
+//     var test = document.getElementById('firstname');
+//     // test.type = "checkbox";
+//     test.name = "second";
+//     // test.disabled = true; //disables the checkbox.
+//     var containerDiv = document.getElementById('container');
+
+//     // function createElement()
+//     // var label = document.createElement('label');
+//     // label.id="lblFirstName";
+//     // label.innerHTML = "Welcome";
+
+//     // containerDiv.appendChild(label);
+
+//     test.readOnly = true;
+// })();
+var myPosts = [{text:'',time:''}]
+function createPost() {
+    var postContent = document.getElementById('post');
+    var postsContainer = document.getElementById('postsContainer');
+    postsContainer.innerHTML='';
+    myPosts.push({
+        text: postContent.value,
+        time: new Date()
+    });
+    for(var i = myPosts.length-1; i >=0; i--) {
+        var myCard = `<div class="card">
+                  
+        <div class="card-body">
+                 ${myPosts[i].text} ${myPosts[i].time}
+        </div>
+    </div>`;
+postsContainer.innerHTML+=myCard;
+    }
+  
+    postContent.value='';            
+}
