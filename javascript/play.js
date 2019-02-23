8b7dfcfda9a53fa9e2a76cf7b212bcb93998c06f
Koushik Mani <koushik-m@Koushiks-MacBook-Pro.local>
2019-02-23 15:34:12 +0530

    added code

--- javascript/play.js
+++ javascript/play.js
@@ -15,24 +15,30 @@
 
 //     test.readOnly = true;
 // })();
-var myPosts = [{text:'',time:''}]
-function createPost() {
-    var postContent = document.getElementById('post');
-    var postsContainer = document.getElementById('postsContainer');
-    postsContainer.innerHTML='';
-    myPosts.push({
-        text: postContent.value,
-        time: new Date()
-    });
-    for(var i = myPosts.length-1; i >=0; i--) {
-        var myCard = `<div class="card">
+// var myPosts = [{ text: '', time: '' }]
+// function createPost() {
+//     var postContent = document.getElementById('post');
+//     var postsContainer = document.getElementById('postsContainer');
+//     postsContainer.innerHTML = '';
+//     myPosts.push({
+//         text: postContent.value,
+//         time: new Date()
+//     });
+//     for (var i = myPosts.length - 1; i >= 0; i--) {
+//         var myCard = `<div class="card">
                   
-        <div class="card-body">
-                 ${myPosts[i].text} ${myPosts[i].time}
-        </div>
-    </div>`;
-postsContainer.innerHTML+=myCard;
-    }
-  
-    postContent.value='';            
-}
+//         <div class="card-body">
+//                  ${myPosts[i].text} ${myPosts[i].time}
+//         </div>
+//     </div>`;
+//         postsContainer.innerHTML += myCard;
+//     }
+
+//     postContent.value = '';
+// }
+(function(){
+    var test = document.getElementById("btn").addEventListener("click",function(){
+        document.getElementById("postsContainer").innerHTML = document.getElementById('post').value;
+      });
+
+})();
