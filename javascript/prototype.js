63e58228404f946e057b7d4bbee272db669bbddc
koushik-m <koushik2498@gmail.com>
2019-03-11 16:54:54 +0530

    added code

---
+++ javascript/prototype.js
@@ -0,0 +1,23 @@
+//prototypal inheritance
+
+let user = {
+    name: "John",
+    surname: "Smith",
+
+    set fullName(value) {
+        [this.name, this.surname] = value.split(" ");
+    },
+
+    get fullName() {
+        return `${this.name} ${this.surname}`;
+    }
+};
+
+let admin = {
+    __proto__: user,
+    isAdmin: true
+};
+
+alert(admin.fullName);
+admin.fullName = "Alice Cooper";
+user.fullName();
